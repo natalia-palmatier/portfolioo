97d059b0c14d236e437bd93bf705b9ed9fe76486
Natalia Palmatier <87651358+natalia-codes@users.noreply.github.com>
2023-01-11 14:00:43 -0800

    email functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import About from "./components/About";
+// import Contact from "./components/Contact";
 import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
--- src/components/About.js
+++ src/components/About.js
@@ -6,15 +6,15 @@ export default function About() {
     <section id="about">
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 id="typewriter">Hi, I'm Natalia ♡ </h1>
-          <h1 id="subheader">Irvine, CA</h1>
+          <h1 id="typewriter">Natalia Palmatier </h1>
+          <h1 id="subheader">Software Engineer</h1>
           <br></br>
           <p className="mb-8 leading-relaxed text-black">
           I am a software engineer with 3 years of experience in the industry. 
           Throughout my career, I have had the opportunity to work on a variety of projects, 
           and enjoy anything challenging that requires deeper thought. 
           I am always striving to improve and deliver high-quality products. 
-          I guess you could say im ~ <em>committed</em> ~ *wink wink*
+          One could even say that I'm ~ <em>committed</em> ~ 
           </p>
           <img src="http://ghchart.rshah.org/f472b6/natalia-codes" alt="Natalia's Pink Github chart" class="my-chart" />
           <div className="flex justify-center">
